0b63f7d6246f4dad6257a7729abe43db10d74509
Just_Kvin <144643334+Kairbros@users.noreply.github.com>
2025-04-08 15:13:38 -0500

    update

--- scenes/menu_script.gd
+++ scenes/menu_script.gd
@@ -2,9 +2,14 @@ extends Control
 
 @onready var max_points_count: RichTextLabel = $Hbox/MaxPointsCount
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var exit_btn: Button = $VBoxContainer/ExitBtn
+
+func _ready() -> void:
+	if OS.has_feature("web"):
+		exit_btn.disabled = true
+		exit_btn.visible = false
 
 func _physics_process(_delta: float) -> void:
-
 	max_points_count.text = str(Global.max_points)
 
 
